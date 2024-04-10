1725f68ad84aa9167c595422cdc8f61646362511
mohammadfarooq02 <mohammad.farooq02@nagarro.com>
2024-04-10 12:43:17 +0530

    Add project files.

---
+++ DotNetDevOps.Tests/Controllers/WeatherForecastControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using DotNetDevOps.Controllers;
+
+namespace DotNetDevOps.Tests.Controllers
+{
+    public class WeatherForecastControllerTest
+    {
+        [Fact]
+        public void Get_ReturnsWeatherForecasts()
+        {
+            // Arranges
+            var controller = new WeatherForecastController();
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+            Assert.IsAssignableFrom<IEnumerable<WeatherForecast>>(result);
+        }
+
+
+    }
+}
