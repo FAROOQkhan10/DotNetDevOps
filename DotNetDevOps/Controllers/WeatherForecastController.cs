eb660c489cb4f7d231b82f15741dd8f837129835
MOHAMMAD FAROOQ <66942957+FAROOQkhan10@users.noreply.github.com>
2024-04-13 23:33:51 +0530

    file change

--- DotNetDevOps/Controllers/WeatherForecastController.cs
+++ DotNetDevOps/Controllers/WeatherForecastController.cs
@@ -13,11 +13,6 @@ namespace DotNetDevOps.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController()
-        {
-
-        }
-
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
